acf42dba6658ace67d4053243577cefb7fe8ea9f
ander9991 <sg8002@nyu.edu>
2024-10-19 17:17:20 -0400

    Edited URL

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "127.0.0.1:8000",
-    "django-app-env.eba-kkmi2cza.us-west-2.elasticbeanstalk.com",
+    "Django-app-env.eba-2kkjqdcc.us-west-2.elasticbeanstalk.com",
     ".elasticbeanstalk.com",
 ]
 
